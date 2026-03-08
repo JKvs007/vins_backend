cd16165be1c5ca5f338ba4f72c5527684534193d
Harikris20 <harivs7139@gmail.com>
2026-03-08 19:41:36 +0530

    Fix Agora Role_Attendee import error

--- services/agora_service.py
+++ services/agora_service.py
@@ -1,23 +1,19 @@
 import time
-from agora_token_builder import RtcTokenBuilder, Role_Attendee
+from agora_token_builder import RtcTokenBuilder, RoleAttendee
 from core.config import settings
 
 
 def generate_agora_token(channel_name: str, uid: int = 0) -> dict:
-    """
-    Generates a temporary Agora token for joining a video/audio call.
-    """
-
     expiration_time_in_seconds = 3600 * 24
-    current_time_stamp = int(time.time())
-    privilege_expired_ts = current_time_stamp + expiration_time_in_seconds
+    current_timestamp = int(time.time())
+    privilege_expired_ts = current_timestamp + expiration_time_in_seconds
 
     token = RtcTokenBuilder.buildTokenWithUid(
         settings.AGORA_APP_ID,
         settings.AGORA_APP_CERTIFICATE,
         channel_name,
         uid,
-        Role_Attendee,
+        RoleAttendee,
         privilege_expired_ts
     )
 
